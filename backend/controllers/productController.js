a7a053976fdd34e9fa187b26b5ced0655c0bed55
yuke001 <yukenthirans2018@gmail.com>
2025-02-10 23:50:04 +0530

    Implement product retrieval: get all & single product by ID

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,10 @@
 import Product from "../model/productModel.js";
 
-// create product
+// create product - /api/v1/products
 export const createProduct = async (req, res, next) => {
   const { name, price, description, categories, seller, stock } = req.body;
 
-  // Validate required fields 
+  // Validate required fields
   if (!name || !price || !description || !categories || !seller || !stock) {
     return next(new Error("Please Provide All Required Fields"));
   }
@@ -17,10 +17,31 @@ export const createProduct = async (req, res, next) => {
   });
 };
 
-// get all products
-export const getPrducts = (req, res) => {
+// get all products - api/v1/products
+export const getPrducts = async (req, res, next) => {
+  const products = await Product.find();
+
   res.status(200).json({
     success: true,
-    message: "Hello World",
+    count: products.length,
+    products,
+  });
+};
+
+// get single product - api/v1/products/:id
+
+export const getSingleProduct = async (req, res, next) => {
+  let { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    // return next(new Error("Product Not Found"));
+    return res.status(404).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
   });
 };
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createProduct, getPrducts } from "../controllers/productController.js";
+import { createProduct, getPrducts, getSingleProduct } from "../controllers/productController.js";
 
 let router = Router();
 
 router.get("/", getPrducts)
+router.get("/:id", getSingleProduct)
 router.post("/", createProduct)
 
 
